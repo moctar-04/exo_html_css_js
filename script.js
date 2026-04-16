3144c0342b2e5598ede9d1f4ded0020dd6485564
moctar04 <amadou.moctar0401@gmail.com>
2026-04-16 14:33:58 +0000

    ajout des exrcices

---
+++ script.js
@@ -0,0 +1,12 @@
+/*exercice 1*/
+
+function changerTexte() {
+    let textchange=document.getElementById("titre");
+    textchange.innerHTML=" Mouhamed est le chef de groupe ";
+    textchange.style.color="blue";
+}
+/*exercice 2 */
+
+function afficherMessage() {
+    alert("vous avez une carte d'invitation")
+}
